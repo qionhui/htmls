7b6b251dfd86665de746867fb7216a1662814684
qionhui <40323604+qionhui@users.noreply.github.com>
2019-04-04 01:43:53 +0800

    Add files via upload

---
+++ draw/script.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+    var css = document.getElementById("css");
+    var textUl = document.getElementsByTagName("ul")[0];
+
+    function play(num) {
+        var width = 600 / num;
+        var cssW  = "ul li,ul li div{width: "+width+"px;}";
+        var cssDiv = "";
+        var Zhtml = "";
+        var z = 1;
+        for (var i = 0;i < num;i++){
+            if(i>num / 2){
+                Zhtml += "ul li:nth-child("+(i+1)+") div{z-index: "+ (z++) +"}";
+            }
+            cssDiv += "ul li:nth-child("+(i+1)+") div{background-position:"+ (-width*i) +"px;" +
+                "background-size: auto  400px" +
+                "}";
+            textUl.innerHTML += "<li><div></div><div></div> <div></div><div></div></li>";
+
+        }
+        css.innerHTML += cssW + cssDiv;
+    }
+
+    function start(){
+        var r = 0;
+        setInterval(function () {
+            var ll = document.getElementsByTagName("li");
+            r+=90;
+            for(var i = 0;i < 60;i++){
+                if(ll[i].style){
+                    // ll[i].style.transform = "rotateX("+r+"deg)";
+                    dh(i,r,i*10);
+                }
+            }
+
+        },3000);
+    }
+    function dh(i,r,time){
+        setTimeout(function () {
+            document.getElementsByTagName("li")[i].style.transform = "rotateX("+r+"deg)";
+        },time);
+    }
+    play(60);
+    start();
+
+
+
+
+}
